6d3bcd708084e9ed1b388f1d5d3c106c1af8b2ef
AlbaMarq <albamarquesr@gmail.com>
2024-01-09 14:12:11 +0100

    Add files via upload

---
+++ Piscina/Rush/RUSH00/alba.c
@@ -0,0 +1,14 @@
+/*#include <unistd.h>*/
+#include <stdio.h>
+
+void    rush(int x, int y)
+{
+    char matriz[y,x];
+    printf(matriz);
+}
+
+int main(void)
+{
+    rush04(3, 3);
+    return (0);
+}
