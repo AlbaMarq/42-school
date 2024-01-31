3b96817930241f8f5e54b24a012afb659ebc4c17
AlbaMarq <albamarquesr@gmail.com>
2024-01-31 21:01:27 +0100

    empiezo ft_printf

---
+++ Cursus/2. printf/libftprintf.h
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libftprintf.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: albmarqu <albmarqu@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/31 20:54:22 by albmarqu          #+#    #+#             */
+/*   Updated: 2024/01/31 20:57:25 by albmarqu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../1. libft/libft.h"
+#include <unistd.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+int	ft_printf(char const *, ...);
