f1cb16cb47063c29fec9ec0b485e4bc40dc618b8
AlbaMarq <albamarquesr@gmail.com>
2024-02-05 13:33:26 +0100

    Actualizar libft.h

--- Cursus/1. libft/libft.h
+++ Cursus/1. libft/libft.h
@@ -10,10 +10,13 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
-#include <stdlib.h>
-#include <ctype.h>
-#include <string.h>
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <ctype.h>
+# include <string.h>
 
 typedef struct s_list
 {
@@ -68,3 +71,5 @@ void	ft_lstdelone(t_list *lst, void (*del)(void *));
 void	ft_lstclear(t_list **lst, void (*del)(void *));
 void	ft_lstiter(t_list *lst, void (*f)(void *));
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
+
+#endif
